4d90753762bc168cd4c375ca494f739bd43b8a4e
Alextopher <christopher.mahoney2000@gmail.com>
2022-09-09 11:12:28 -0400

    structure project

---
+++ target/debug/deps/itlcd-f22e3a1d5d63f15f.d
@@ -0,0 +1,5 @@
+/home/mahonec/p/github.com/COSI-Lab/itl-chorus/target/debug/deps/itlcd-f22e3a1d5d63f15f.rmeta: itlcd/src/main.rs
+
+/home/mahonec/p/github.com/COSI-Lab/itl-chorus/target/debug/deps/itlcd-f22e3a1d5d63f15f.d: itlcd/src/main.rs
+
+itlcd/src/main.rs:
