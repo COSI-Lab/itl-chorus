3048f0bb90addb3f59c77dd26d9b1fe858fac186
sophiacarlone <carlonsm@clarkson.edu>
2022-11-01 22:04:12 -0400

    set up two functions in midi.rs to work on later

--- itlcd/src/midi.rs
+++ itlcd/src/midi.rs
@@ -1,1 +1,9 @@
 //start of midi operations
+
+fn verify_is_midi(file: &String) -> Bool{
+    //function used to verify file is in midi format
+}
+
+fn parse_midi(){
+    //function used to parse file and create a midi data structure
+}
