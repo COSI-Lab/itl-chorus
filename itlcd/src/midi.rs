d99728e5d06cfeda0e91db82b069f2712d90403c
sophiacarlone <carlonsm@clarkson.edu>
2022-11-11 13:46:48 -0500

    push up everything

--- itlcd/src/midi.rs
+++ itlcd/src/midi.rs
@@ -2,8 +2,9 @@
 
 fn verify_is_midi(file: &str) -> Bool{
     //function used to verify file is in midi format
+    
 }
 
-fn parse_midi(){
+fn parse_midi(file: &str){
     //function used to parse file and create a midi data structure
 }
