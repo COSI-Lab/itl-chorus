d218d31425799b36ed9a74b99231d151c258332b
Alextopher <christopher.mahoney2000@gmail.com>
2023-06-24 15:09:53 -0400

    use local soundfont

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 function play() {
-    JZZ.synth.MIDIjs({ soundfontUrl: "https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/", instrument: "acoustic_grand_piano" })
+    JZZ.synth.MIDIjs({ soundfontUrl: "/sf/", instrument: "acoustic_grand_piano" })
         .note(0, 'C5', 127, 500).wait(500)
         .note(0, 'E5', 127, 500).wait(500)
         .note(0, 'G5', 127, 500).wait(500)
